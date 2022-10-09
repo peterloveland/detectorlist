1028032b0b3c88355925d887cf0fe9256ac4e8e8
Peter Loveland <peterloveland@github.com>
2022-10-09 20:22:46 +0100

    Initial Commit

---
+++ Detectorlist/DetectorlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectorlistApp.swift
+//  Detectorlist
+//
+//  Created by Peter Loveland on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DetectorlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
